efac3c194230a4dab57793842a3f544594a42079
Shashi Kumar <shashi.kumar11@ad.infosys.com>
2018-03-29 14:17:22 +0530

    removed dropout added data aug

--- drive.py
+++ drive.py
@@ -11,6 +11,7 @@ import eventlet.wsgi
 from PIL import Image
 from flask import Flask
 from io import BytesIO
+import tensorflow as tf
 
 from keras.models import load_model
 import h5py
@@ -44,7 +45,7 @@ class SimplePIController:
 
 
 controller = SimplePIController(0.1, 0.002)
-set_speed = 5
+set_speed = 3
 controller.set_desired(set_speed)
 
 
@@ -63,7 +64,7 @@ def telemetry(sid, data):
         image_array = np.asarray(image)
         #CROPPED and RESIZE
         image_array=image_array[60:140,:,:]
-        image_array=cv2.resize(image_array, (64,64))
+        #image_array=cv2.resize(image_array, (64,64))
         
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
 
--- model_gen.py
+++ model_gen.py
@@ -13,44 +13,43 @@ with open('./data/driving_log.csv') as csvfile:
 			first = False
 			continue
 		lines.append(line)
+		
+		
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
 
-images = []
-measurements = []
-LEFT_CORR = 0.1
-RIGHT_CORR = 0.2
-DROP_RATE = 0.50
-	
+            images = []
+            measurements = []
+            for batch_sample in batch_samples:
+                name =  './data/IMG/'+batch_sample[0].split('/')[-1]
+                center_image = cv2.imread(name)
+                center_angle = float(batch_sample[3])
+                images.append(center_image)
+                measurements.append(center_angle)
+
+            # trim image to only see section with road
+            X_train = np.array(images)
+            y_train = np.array(measurements)
+            yield sklearn.utils.shuffle(X_train, y_train)
+
 for line in lines:
-	centre_file = './data/IMG/'+ line[0].split('/')[-1]
-	left_file   = './data/IMG/'+ line[1].split('/')[-1]
-	right_file  = './data/IMG/'+ line[2].split('/')[-1]
-	if np.random.uniform() < DROP_RATE:
-		image = cv2.imread(centre_file )
-		image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-		image =image[60:140,:,:]
-		measurement = float(line[3])
-		images.append(image)
-		measurements.append(measurement)
-	
+	source_file = line[0]
+	filename = source_file.split('/')[-1]
+	current_path =  './data/IMG/'+ filename
+	image = cv2.imread(current_path)
+	image=cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+	image =image[60:140,:,:]
 	#image =cv2.resize(image, (64,64))
-	#if measurement > -0.05 && measurement < 0.05:
-	image = cv2.imread(left_file )
-	image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-	image =image[60:140,:,:]
-	measurement = float(line[3]) + LEFT_CORR
-	images.append(image)
-	measurements.append(measurement)
-	
-	image = cv2.imread(right_file )
-	image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-	image =image[60:140,:,:]
-	measurement = float(line[3]) - RIGHT_CORR
-	images.append(image)
-	measurements.append(measurement)
-	
-	
     
-	
+	images.append(image)
+
+	measurement = float(line[3])
+	measurements.append(measurement)
     
 X_train = np.array(images)
 y_train = np.array(measurements)
@@ -61,12 +60,15 @@ print('y_train.shape',y_train.shape)
 
 # Crop done in Drive.py
 
-
 # Resize
+#def resize(x):
+#	return tf.image.resize_images(x,[64,64])
+#X_train = tf.image.resize_images(X_train, [64,64])
+
 def resize(X):
 	#return ktf.resize_images(X,64.0/80, 64.0/320, "channels_first")
 	import tensorflow as tf
-	return tf.image.resize_images(X,[32,128])
+	return tf.image.resize_images(X,[64,64])
 
 
 
@@ -81,23 +83,23 @@ model = Sequential()
 model.add(Lambda(lambda x: x/127.5 - 1,input_shape = (80,320,3)))
 model.add(Lambda(resize))
 
-model.add(Convolution2D(6,3,3))
+model.add(Convolution2D(6,5,5))
 model.add(Activation('relu'))
 
-model.add(Convolution2D(12,3,3))
+model.add(Convolution2D(12,5,5))
 model.add(Activation('relu'))
 
-#model.add(Dropout(0.5))
+model.add(Dropout(0.5))
 
 model.add(Flatten())
 
 model.add(Dense(300))
 model.add(Activation('relu'))
-#model.add(Dropout(0.5))
+model.add(Dropout(0.5))
 
 model.add(Dense(100))
 model.add(Activation('relu'))
-#model.add(Dropout(0.5))
+model.add(Dropout(0.5))
 
 model.add(Dense(30))
 model.add(Activation('relu'))
@@ -106,7 +108,7 @@ model.add(Dense(1))
 
 
 model.compile(loss = 'mse', optimizer = 'adam')
-history_object = model.fit(X_train, y_train, validation_split = 0.2, shuffle = True, nb_epoch = 2, batch_size = 128)
+history_object = model.fit(X_train, y_train, validation_split = 0.2, shuffle = True, nb_epoch = 2)
 ### print the keys contained in the history object
 print(history_object.history.keys())
 
